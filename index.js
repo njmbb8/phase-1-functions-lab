123657fa2049161313430d59e233abd31e4e4597
njmbb8 <nmcgr001@gmail.com>
2021-06-07 20:58:13 -0400

    commit tree a79c27ccffd937d2e72719a6816186db942398f6

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(streetNum) {
+    return Math.abs(42 - streetNum);
+}
+function distanceFromHqInFeet(streetNum) {
+    const blocks = distanceFromHqInBlocks(streetNum);
+    const feetPerBlock = 264;
+    return blocks * feetPerBlock;
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start, end) {
+    const feetActual = distanceTravelledInFeet(start, end);
+    let fare;
+    if(feetActual <= 400){
+        fare = 0;
+    }
+    else{
+        let feetPaid = feetActual - 400;
+        if(feetActual > 400 && feetActual <= 2000){
+            fare = feetPaid * .02;
+        }
+        else if(feetActual > 2000 && feetActual < 2500){
+            fare = 25;
+        }
+        else{
+            fare = 'cannot travel that far';
+        }
+    }
+    return fare;
+}
